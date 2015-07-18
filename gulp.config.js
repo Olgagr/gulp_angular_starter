93835b9e5310c03befe33fae3b250f95342c9f4e
APPLE <apple@MacBook-Pro-APPLE.local>
2015-07-18 07:29:19 +0200

    add styles and styles-watch tasks

--- gulp.config.js
+++ gulp.config.js
@@ -1,12 +1,19 @@
 module.exports = function() {
 
+  var src = './src/',
+      tmp = './.tmp';
+
   var config = {
+
+    tmp: tmp,
 
     // all js for vet
     alljs : [
       './src/**/*.js',
       './*.js'
-    ]
+    ],
+
+    scss: [src + 'assets/styles/**/*.scss']
   };
 
   return config;
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     args = require('yargs').alias('v', 'verbose').argv,
     $ = require('gulp-load-plugins')({lazy:true}),
+    del = require('del'),
     config = require('./gulp.config')();
 
 gulp.task('vet', function() {
@@ -16,7 +17,35 @@ gulp.task('vet', function() {
     .pipe($.jshint.reporter('fail'));
 });
 
+gulp.task('styles', ['clean-styles'], function() {
+
+  log('Compiling SCSS --> CSS');
+
+  return gulp
+    .src(config.scss)
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+      .pipe($.sass())
+    .pipe($.sourcemaps.write())
+    .pipe($.autoprefixer())
+    .pipe(gulp.dest(config.tmp));
+});
+
+gulp.task('clean-styles', function(done) {
+  var files = config.tmp + '**/*.css';
+  clean(files, done);
+});
+
+gulp.task('styles-watch', function(){
+  gulp.watch([config.scss], ['styles']);
+});
+
 //////
+
+function clean(path, done) {
+  log('Cleaning ' + path);
+  del(path, done);
+}
 
 function log(msg) {
   if (typeof(msg) === 'object') {
